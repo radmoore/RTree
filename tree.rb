468ea1d014ff474702f1e74eb93b2f2322af4419
Andrew Moore <radmoore@gmail.com>
2010-03-11 11:38:06 +0100

    * added lots of wrappers, working on preoder traversal

--- tree.rb
+++ tree.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/ruby -w
 
 # VERSION Wed Mar 10 11:04:54 CET 2010
 
@@ -8,14 +8,17 @@
 class Tree
 
   def initialize(lineage, polytomy=false)
-    @nodes = Array.new
+    @nodes = Hash.new
     @polytomy = polytomy
     @sep = "\t" # separator in to_lineage // to_s
     @traversal_strategy = 'pre'
+    @current_node = nil
+    @root_node = nil
+    @visited = Hash.new
     parse(lineage)
   end
 
-  attr_reader :polytomy
+  attr_reader :polytomy, :root_node
   attr_accessor :sep
 
   # return string representation
@@ -36,20 +39,100 @@ class Tree
     end
   end
 
+  # return subtree with current node
+  # as root, or nil if child
+  def prune(node_name)
+
+  end
+
+  # prune in place
+  def prune!(node_name)
+    
+  end
+
+  def visited?(node)
+    @visited.has_key?(node.name)
+  end
+
+  def has_nodes?
+   @visited.keys.size < @nodes.keys.size ? true : false
+  end
+
+  def node_exists?(node_name)
+    @nodes.has_key?(node_name)
+  end
+
   def add_node(name)
     node = Node.new(name)
-    @nodes.push(node)
+    @nodes[name] = node
     return node
   end
 
-  # return true if tree still has nodes (traversal)
-  def has_nodes
-
+  def set_traversal_strategy(s="pre")
+    raise "*** E: only preoder traversal (pre) currently supported" unless (s == "pre")
+    @traversal_strategy = s
   end
 
-  # traverse the tree
-  def traverse
+  def get_node(node_name)
+    raise "*** E: Attempt to access undefined node #{node_name}" unless (self.node_exists?(node_name))
+    @nodes[node_name]
   end
+
+  def get_child_nodes(node_name)
+    @nodes[node_name].children
+  end
+
+  def get_parent_node(node_name)
+    @nodes[node_name].parent
+  end
+
+  # reset current_node to root
+  def reset
+    @current_node = @root_node
+  end
+
+  # return next node given the
+  # traversal strategy
+  def next_node
+    node = @cnode
+    puts "\t\tSetting this node to seen: #{node}"
+    @visited[node.name] = 0
+    puts "STARTING TRAVERSAL"
+    puts "=================================="
+    @cnode = pre_traversal(node)
+    return node
+  end
+
+  # cases:
+  # is node leaf
+  # have we visited its sibling
+  # does node have children (ie. not leaf)
+  # have we visited each child?
+  def pre_traversal(node)
+    if node.is_leaf?
+      puts "\t\t\tNow looking here: leaf node #{node}"
+      if (self.visited?(node))
+        puts "VISITED? #{self.visited?(node)}"
+        puts "\t\t\tI have also been here before: #{node}"
+      end
+      return node unless self.visited?(node)
+      pre_traversal(node.parent)
+    else
+      puts "\t\t\tInternal node: "+node.to_s
+      if (self.visited?(node.left_child))
+        puts "VISITED? #{self.visited?(node.left_child)}"
+        puts "\t\t\tI have been here before: #{node.left_child}"
+      else (self.visited?(node.right_child))
+        puts "VISITED? #{self.visited?(node.right_child)}"
+        puts "\t\t\tI have been here before: #{node.right_child}"
+      end
+      return node.left_child unless self.visited?(node.left_child)
+      return node.right_child unless self.visited?(node.right_child)
+      pre_traversal(node.parent)
+        
+    end
+  end
+
 
   # returns lineage for species
   def to_lineage(species)
@@ -62,11 +145,12 @@ class Tree
 
   end
 
+
+
   private
 
   # parse lineage file
   def parse(lineage)
-
     begin
       f = File.open(lineage)
     rescue
@@ -82,15 +166,18 @@ class Tree
       pnode = nil
       l_nodes.each do |n|
         n = n.split(': ')[1] if (n.include?(':'))
-        node = self.add_node(n)
-        unless (pnode.nil?)
+        node = self.node_exists?(n) ? @nodes[n] : self.add_node(n)
+        if (not pnode.nil?)
           node.add_parent_node(pnode)
           pnode.add_child_node(node)
+        else
+          @root_node = node
         end
         pnode = node
       end
     end
      f.close
+     @cnode = @root_node
   end
 
 end
@@ -104,11 +191,15 @@ class Node
     @associations = nil
   end
 
-  attr_reader :name, :parent, :associations
+  attr_reader :name, :parent, :children, :associations
 
   # add a child node
   def add_child_node(child_node)
     @children << child_node
+  end
+
+  def has_children?
+    not @children.nil?
   end
 
   # add a parent node
@@ -118,15 +209,33 @@ class Node
 
   # return true if node is leaf
   def is_leaf?
-    @children.nil? ? true : false
+    @children.empty? ? true : false
   end
 
   def is_root?
     @parent.nil? ? true : false
   end
 
+  def left_child
+    return nil if self.is_leaf?
+    self.children[0]
+  end
+
+  def right_child
+    return nil if self.is_leaf?
+    self.children[1]
+  end
+
   # associate object with node
   def assoc_with_node
+  end
+
+  # return sister taxa, 
+  # or root of neighbouring subtree
+  # or nil if self is root node
+  def get_sibling
+    return nil if self.is_root?
+    @parent.children.each {|child| return child unless (child == self) }
   end
 
   def to_s
@@ -137,4 +246,8 @@ end
 
 
 t = Tree.new(ARGV[0])
-t.to_s
+
+while (t.has_nodes?)
+  puts t.next_node
+end
+
